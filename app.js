c5c4f8443dcf87c83c6a7dfd5d5348ac8c8e9846
Jake <jake0oo0andminecraft@gmail.com>
2014-10-19 12:00:32 -0500

    No need for unused routes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var avatars = require('./routes/avatars')
 
 var app = express();
@@ -24,7 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/avatars', avatars)
 
 // catch 404 and forward to error handler
