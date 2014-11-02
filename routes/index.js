b9848e6e1f228484c339ebf3240ebf606e0fea71
jomo <github@jomo.tv>
2014-11-02 14:54:57 +0100

    show commit hash on site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Crafatar', domain: "https://" + req.headers.host });
+  res.render('index', { title: 'Crafatar', domain: "https://" + req.headers.host, commit: process.env.COMMIT_HASH || "unknown" });
 });
 
 
