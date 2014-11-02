9e46878932f5c80e01f2ef458dfe2dfbeeba4f3e
jomo <github@jomo.tv>
2014-11-02 04:40:06 +0100

    much secure hyper text transfer protocol. wow :dog2:

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Crafatar', domain: req.protocol + "://" + req.headers.host });
+  res.render('index', { title: 'Crafatar', domain: "https://" + req.headers.host });
 });
 
 
