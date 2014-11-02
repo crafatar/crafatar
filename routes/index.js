19597e232db16f861aa157a172ebf897ec994c6f
jomo <github@jomo.tv>
2014-11-02 16:04:11 +0100

    get commit hash from post-commit hook, see http://stackoverflow.com/a/14924922/2517068

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Crafatar', domain: "https://" + req.headers.host, commit: process.env.COMMIT_HASH || "unknown" });
+  res.render('index', {
+    title: 'Crafatar',
+    domain: "https://" + req.headers.host,
+    // see http://stackoverflow.com/a/14924922/2517068
+    commit: process.env.HEAD_HASH || "unknown"
+  });
 });
 
 
