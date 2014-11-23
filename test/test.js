bac6a0dadde718ef632f1c6f2b130a9ee2c756b9
jomo <github@jomo.tv>
2014-11-23 23:05:43 +0100

    less spammy tests :)

--- test/test.js
+++ test/test.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 
 var networking = require('../modules/networking');
 var helpers = require('../modules/helpers');
+var logging = require('../modules/logging');
 var config = require('../modules/config');
 var skins = require('../modules/skins');
 var cache = require("../modules/cache");
 
 // we don't want tests to fail because of slow internet
 config.http_timeout = 3000;
+
+// no spam
+logging.log = function(){};
 
 var uuids = fs.readFileSync('test/uuids.txt').toString().split("\n");
 var usernames = fs.readFileSync('test/usernames.txt').toString().split("\n");
