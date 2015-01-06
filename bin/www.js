28ad09213ca2bc3927a94ede1a3b4dedd80ca349
jomo <github@jomo.tv>
2015-01-06 18:44:27 +0100

    fix indentation

--- bin/www.js
+++ bin/www.js
@@ -4,17 +4,17 @@ var config = require("../modules/config");
 var cluster = require("cluster");
 
 if (cluster.isMaster) {
-    var cores = require("os").cpus().length;
-    for (var i = cores; i > 0; i--) {
-        cluster.fork();
-    }
+  var cores = require("os").cpus().length;
+  for (var i = cores; i > 0; i--) {
+    cluster.fork();
+  }
 
-    cluster.on("exit", function (worker, code, signal) {
-        logging.error("Worker died. Rebooting a new one.");
-        cluster.fork();
-    });
+  cluster.on("exit", function (worker, code, signal) {
+    logging.error("Worker died. Rebooting a new one.");
+    cluster.fork();
+  });
 
-    setInterval(cleaner.run, config.cleaning_interval * 1000);
+  setInterval(cleaner.run, config.cleaning_interval * 1000);
 } else {
-    require("../server.js")();
+  require("../server.js")();
 }
