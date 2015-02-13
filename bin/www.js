crafatar/crafatar

commit af421ecb49b96e4279e7202c18084c02bab01acb

Start http.server tests

server.js
@@ -7,6 +7,7 @@ var http = require("http");
 var mime = require("mime");
 var url = require("url");
 var fs = require("fs");
+var server = null;
 
 var routes = {
   index: require("./routes/index"),
@@ -53,22 +54,22 @@ function requestHandler(req, res) {
     try {
       switch (local_path) {
         case "":
-          routes.index(request, res);
-          break;
+        routes.index(request, res);
+        break;
         case "avatars":
-          routes.avatars(request, res);
-          break;
+        routes.avatars(request, res);
+        break;
         case "skins":
-          routes.skins(request, res);
-          break;
+        routes.skins(request, res);
+        break;
         case "renders":
-          routes.renders(request, res);
-          break;
+        routes.renders(request, res);
+        break;
         case "capes":
-          routes.capes(request, res);
-          break;
+        routes.capes(request, res);
+        break;
         default:
-          asset_request(request, res);
+        asset_request(request, res);
       }
     } catch(e) {
       var error = JSON.stringify(req.headers) + "\n" + e.stack;
@@ -86,15 +87,27 @@ function requestHandler(req, res) {
   }
 }
 
-var boot = module.exports = function () {
+var exp = {};
+
+exp.boot = function(callback) {
   var port = process.env.PORT || 3000;
   var bind_ip = process.env.BIND || "127.0.0.1";
   console.log("Server running on http://" + bind_ip + ":" + port + "/");
-  http.createServer(requestHandler).listen(port, bind_ip);
+  server = http.createServer(requestHandler).listen(port, bind_ip, function() {
+    callback();
+  });
 };
 
+exp.close = function(callback) {
+  server.close(function() {
+    callback();
+  });
+}
+
+module.exports = exp;
+
 if (require.main === module) {
-  boot();
+  exp.boot(function(){});
 
   // cleaning worker
   setInterval(clean.run, config.cleaning_interval * 1000);

test/test.js
@@ -8,7 +8,8 @@ var config = require("../modules/config");
 var skins = require("../modules/skins");
 var cache = require("../modules/cache");
 var renders = require("../modules/renders");
-var cleaner = require("../modules/cleaner");
+var server = require("../server");
+var request = require("request");
 
 // we don't want tests to fail because of slow internet
 config.http_timeout *= 3;
@@ -33,7 +34,7 @@ var ids = [
   uuid.toLowerCase(),
   name.toLowerCase(),
   uuid.toUpperCase(),
-  name.toUpperCase()
+  name.toUpperCase(),
 ];
 
 describe("Crafatar", function() {
@@ -42,7 +43,6 @@ describe("Crafatar", function() {
 
   before(function() {
     cache.get_redis().flushall();
-    cleaner.run();
   });
 
   describe("UUID/username", function() {
@@ -330,4 +330,63 @@ describe("Crafatar", function() {
       });
     })(id, id_type);
   }
+  describe("Server", function() {
+    before(function(done) {
+      server.boot(function() {
+        done();
+      });
+    });
+
+    // Test the home page
+    it("should return a 200", function(done) {
+      request.get('http://localhost:3000', function(error, res, body) {
+        assert.equal(200, res.statusCode);
+        done();
+      });
+    });
+
+    // invalid method, we only allow GET and HEAD requests
+    it("should return a 405", function(done) {
+      request.post('http://localhost:3000/avatars/Jake0oo0', function(error, res, body) {
+        assert.equal(405, res.statusCode);
+        done();
+      });
+    });
+
+    it("should return a 422 (invalid scale)", function(done) {
+      var scale = config.max_scale + 1;
+      request.get('http://localhost:3000/avatars/Jake0oo0?scale=' + scale, function(error, res, body) {
+        assert.equal(422, res.statusCode);
+        done();
+      });
+    });
+
+    it("should return a 422 (invalid uuid)", function(done) {
+      request.get('http://localhost:3000/avatars/thisisaninvaliduuid', function(error, res, body) {
+        assert.equal(422, res.statusCode);
+        done();
+      });
+    });
+
+    it("should return a 422 (invalid size)", function(done) {
+      var size = config.max_size + 1;
+      request.get('http://localhost:3000/renders/Jake0oo0?size=' + size, function(error, res, body) {
+        assert.equal(422, res.statusCode);
+        done();
+      });
+    });
+
+    it("should return a 301 (default image)", function(done) {
+      request.get('http://localhost:3000/renders/invalidjsvns?def=steve', function(error, res, body) {
+        assert.equal(301, res.statusCode);
+        done();
+      });
+    });
+
+    after(function(done) {
+      server.close(function() {
+        done();
+      })
+    });
+  });
 });

commit 99a5a34f3bd202e7a370477c0a1344ca9e8f3d9c

fix server not starting
broken by af421ec

bin/www.js
@@ -17,5 +17,5 @@ if (cluster.isMaster) {
 
   setInterval(cleaner.run, config.cleaning_interval * 1000);
 } else {
-  require("../server.js")();
+  require("../server.js").boot();
 }

public/stylesheets/style.css
@@ -111,7 +111,7 @@ h4 {
 }
 
 .code .example {
-  cursor: pointer;
+  cursor: text;
 }
 
 .code .example:hover {

server.js
@@ -94,7 +94,9 @@ exp.boot = function(callback) {
   var bind_ip = process.env.BIND || "127.0.0.1";
   logging.log("Server running on http://" + bind_ip + ":" + port + "/");
   server = http.createServer(requestHandler).listen(port, bind_ip, function() {
-    callback();
+    if (callback) {
+      callback();
+    }
   });
 };
 
@@ -102,12 +104,12 @@ exp.close = function(callback) {
   server.close(function() {
     callback();
   });
-}
+};
 
 module.exports = exp;
 
 if (require.main === module) {
-  exp.boot(function(){});
+  exp.boot();
 
   // cleaning worker
   setInterval(clean.run, config.cleaning_interval * 1000);